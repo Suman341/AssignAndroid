ce9fee684a2239680832e809f641a1c87df1895a
Suman341 <55776175+Suman341@users.noreply.github.com>
2019-12-22 20:15:03 +0545

    add login design

---
+++ app/src/main/java/com/softwarica/printstation/ui/auth/LoginActivity.java
@@ -0,0 +1,20 @@
+package com.softwarica.printstation.ui.auth;
+import android.os.Bundle;
+import android.view.View;
+import androidx.appcompat.app.AppCompatActivity;
+import com.softwarica.printstation.R;
+
+public class LoginActivity extends AppCompatActivity {
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        
+        setContentView(R.layout.activity_login);
+
+
+    }
+};
+
