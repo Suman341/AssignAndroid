5ee31481f9dbf343c387e6387e02b077767cd722
Suman341 <55776175+Suman341@users.noreply.github.com>
2019-12-25 20:22:16 +0545

    add signup design

---
+++ app/src/main/java/com/softwarica/printstation/ui/auth/SignUpActivity.java
@@ -0,0 +1,16 @@
+package com.softwarica.printstation.ui.auth;
+import android.os.Bundle;
+import android.view.View;
+import androidx.appcompat.app.AppCompatActivity;
+import com.softwarica.printstation.R;
+
+public class SignUpActivity extends AppCompatActivity implements View.OnClickListener {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_up);
+
+
+    }
