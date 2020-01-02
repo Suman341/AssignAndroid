3e7ff3b6dd8573f633fbade2469d8998d343e70f
Suman341 <55776175+Suman341@users.noreply.github.com>
2020-01-02 20:09:48 +0545

    add contact fragment

---
+++ app/src/main/java/com/softwarica/printstation/ui/dashboard/home/contact/ContactUsFragment.java
@@ -0,0 +1,39 @@
+package com.softwarica.printstation.ui.dashboard.home.contact;
+
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.content.pm.ResolveInfo;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageButton;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import com.google.android.gms.maps.CameraUpdate;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.maps.model.MarkerOptions;
+import com.softwarica.printstation.R;
+
+import java.util.List;
+
+public class ContactUsFragment extends Fragment implements OnMapReadyCallback {
+
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_contact_us, container, false);
+
+
+    }
