cfcdc696ef94b0eec631f1e0f87b1ca57990a5b1
Suman341 <55776175+Suman341@users.noreply.github.com>
2020-01-02 22:53:48 +0545

    update contact

--- app/src/main/java/com/softwarica/printstation/ui/dashboard/home/contact/ContactUsFragment.java
+++ app/src/main/java/com/softwarica/printstation/ui/dashboard/home/contact/ContactUsFragment.java
@@ -35,5 +35,57 @@ public class ContactUsFragment extends Fragment implements OnMapReadyCallback {
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_contact_us, container, false);
 
+        ImageButton fbBtn = view.findViewById(R.id.fbBtn);
+        ImageButton instaBtn = view.findViewById(R.id.instsaBtn);
 
+
+        fbBtn.setOnClickListener(v -> {
+            startActivity(getOpenFacebookIntent(getContext(), "368711697049577", "printstationnepal"));
+        });
+        instaBtn.setOnClickListener(v -> {
+            startActivity(getOpenInstagramIntent(getContext(), "printstationnepal"));
+        });
+
+        SupportMapFragment mapFragment = (SupportMapFragment) getChildFragmentManager().findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+
+        return view;
     }
+
+    public Intent getOpenInstagramIntent(Context context, String userName) {
+        try {
+            context.getPackageManager().getPackageInfo("com.instagram.android", 0);
+            return new Intent(Intent.ACTION_VIEW, Uri.parse("http://instagram.com/_u/" + userName));
+        } catch (Exception e) {
+            return new Intent(Intent.ACTION_VIEW, Uri.parse("http://instagram.com/" + userName));
+        }
+    }
+
+    private boolean isIntentAvailable(Context ctx, Intent intent) {
+        final PackageManager packageManager = ctx.getPackageManager();
+        List<ResolveInfo> list = packageManager.queryIntentActivities(intent, PackageManager.MATCH_DEFAULT_ONLY);
+        return list.size() > 0;
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+
+        drawMarker();
+    }
+
+    private void drawMarker() {
+        if (mMap == null) return;
+
+        if (printStationNepalMarker == null) {
+            printStationNepalMarker = mMap.addMarker(new MarkerOptions().position(printStationPosition).title("PrintStation Nepal"));
+        }else{
+            printStationNepalMarker.setPosition(printStationPosition);
+        }
+        CameraUpdate center = CameraUpdateFactory.newLatLng(printStationPosition);
+        CameraUpdate zoom = CameraUpdateFactory.zoomTo(16);
+
+        mMap.moveCamera(center);
+        mMap.animateCamera(zoom);
+    }
+}
