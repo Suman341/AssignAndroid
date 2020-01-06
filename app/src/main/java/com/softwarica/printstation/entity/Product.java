154a685a00b6eb75e8030475549945169a9a7d31
Suman341 <55776175+Suman341@users.noreply.github.com>
2020-01-06 23:18:59 +0545

    add product entity

---
+++ app/src/main/java/com/softwarica/printstation/entity/Product.java
@@ -0,0 +1,54 @@
+package com.softwarica.printstation.entity;
+
+import com.softwarica.printstation.api.API;
+
+public class Product {
+    private String _id;
+    private String name;
+    private long price;
+    private String image;
+    private int quantity;
+
+    public Product() {
+    }
+
+    public String get_id() {
+        return _id;
+    }
+
+    public void set_id(String _id) {
+        this._id = _id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public long getPrice() {
+        return price;
+    }
+
+    public void setPrice(long price) {
+        this.price = price;
+    }
+
+    public String getImage() {
+        return API.IMAGE_URL + image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+}
