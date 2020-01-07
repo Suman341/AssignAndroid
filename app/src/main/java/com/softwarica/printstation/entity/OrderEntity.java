1657bc9263f224b92456fa454511819a56399859
Suman341 <55776175+Suman341@users.noreply.github.com>
2020-01-07 21:19:24 +0545

    add order entity

---
+++ app/src/main/java/com/softwarica/printstation/entity/OrderEntity.java
@@ -0,0 +1,81 @@
+
+package com.softwarica.printstation.entity;
+
+import com.google.gson.annotations.Expose;
+
+
+public class OrderEntity {
+
+    @Expose
+    private String _id;
+    @Expose
+    private String createdAt;
+    @Expose
+    private String productId;
+    @Expose
+    private Long quantity;
+    @Expose
+    private String userId;
+
+    @Expose
+    private String status;
+
+    @Expose
+    private Product product;
+
+    public String get_id() {
+        return _id;
+    }
+
+    public void set_id(String _id) {
+        this._id = _id;
+    }
+
+    public String getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(String createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public String getProductId() {
+        return productId;
+    }
+
+    public void setProductId(String productId) {
+        this.productId = productId;
+    }
+
+    public Long getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(Long quantity) {
+        this.quantity = quantity;
+    }
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+
+    public Product getProduct() {
+        return product;
+    }
+
+    public void setProduct(Product product) {
+        this.product = product;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+}
