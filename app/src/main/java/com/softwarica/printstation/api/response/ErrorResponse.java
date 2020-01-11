c3743a8ebe7a405b6c560ee1f27bad07247a5dfc
Suman341 <55776175+Suman341@users.noreply.github.com>
2020-01-11 22:06:30 +0545

    err respong handling

---
+++ app/src/main/java/com/softwarica/printstation/api/response/ErrorResponse.java
@@ -0,0 +1,34 @@
+package com.softwarica.printstation.api.response;
+
+public class ErrorResponse {
+    private String name;
+    private String type;
+    private String message;
+
+    public ErrorResponse() {
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+}
