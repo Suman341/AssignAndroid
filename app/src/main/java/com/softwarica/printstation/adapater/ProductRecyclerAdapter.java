8152774672c88871e9a1c83ac46a86b6bd061651
Suman341 <55776175+Suman341@users.noreply.github.com>
2020-01-30 23:56:25 +0545

    functioning product adapter

--- app/src/main/java/com/softwarica/printstation/adapater/ProductRecyclerAdapter.java
+++ app/src/main/java/com/softwarica/printstation/adapater/ProductRecyclerAdapter.java
@@ -17,13 +17,13 @@ import com.squareup.picasso.Picasso;
 import java.util.List;
 
 
-public class ProductRecyclerAdapter {
+public class ProductRecyclerAdapter extends RecyclerView.Adapter<ProductRecyclerAdapter.Holder> {
 
     public interface OnProductAction {
         void onClicked(Product product);
     }
 
-    public interface OnOrderChanged {
+    public interface OnOrderChanged{
         void onChanged(List<Product> orders);
     }
 
@@ -39,6 +39,55 @@ public class ProductRecyclerAdapter {
 
     public void updateProducts(List<Product> products) {
         this.products = products;
+        notifyDataSetChanged();
+    }
 
+    @NonNull
+    @Override
+    public Holder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.product_item, parent, false);
+        return new Holder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull Holder holder, int position) {
+        Product product = products.get(position);
+
+        holder.productName.setText(product.getName());
+        holder.price.setText("Price: "+product.getPrice());
+
+        Picasso.with(holder.itemView.getContext())
+                .load(product.getImage())
+                .into(holder.productImage);
+
+        holder.buyProduct.setOnClickListener(v -> {
+            if (null != this.onProductBuy) onProductBuy.onClicked(product);
+        });
+        holder.addToCart.setOnClickListener(v ->{
+            if (null != this.addToCart) addToCart.onClicked(product);
+        });
+    }
+
+    @Override
+    public int getItemCount() {
+        return products.size();
+    }
+
+    class Holder extends RecyclerView.ViewHolder {
+
+        private ImageView productImage;
+        private TextView productName;
+        private TextView price;
+        private Button addToCart;
+        private Button buyProduct;
+
+        private Holder(@NonNull View itemView) {
+            super(itemView);
+            productImage = itemView.findViewById(R.id.productImage);
+            productName = itemView.findViewById(R.id.productName);
+            price = itemView.findViewById(R.id.price);
+            addToCart = itemView.findViewById(R.id.addToCart);
+            buyProduct = itemView.findViewById(R.id.buyProduct);
+        }
     }
 }
